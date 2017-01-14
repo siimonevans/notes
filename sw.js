c08b492b212f7ffb2d63a152edc3b9f046276c94
Simon Evans <simon.evans@torchbox.com>
2017-01-14 17:06:42 +0000

    Improve ServiceWorker

--- sw.js
+++ sw.js
@@ -1,68 +1,41 @@
+// Polyfill for Chrome caching
 importScripts('js/cache-polyfill.js');
 
-var CACHE_VERSION = 'app-v1';
-var CACHE_FILES = [
-    '/',
-    'index.html',
-    'js/app.js',
-    'js/jquery.min.js',
-    'js/cache-polyfill.js',
-    'css/style.css',
-    'favicon.ico',
-    'manifest.json',
-    'img/icon-60.png',
-    'img/icon-114.png',
-    'img/icon-152.png'
-];
+// Install the ServiceWorker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
 
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_VERSION)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(CACHE_FILES);
-            })
-    );
-});
+    // Open a cache
+    caches.open('v1').then(function(cache) {
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function(res){
-            if(res){
-                return res;
-            }
-            requestBackend(event);
-        })
-    )
-});
-
-function requestBackend(event){
-    var url = event.request.clone();
-    return fetch(url).then(function(res){
-
-        // If not a valid response send the error
-        if(!res || res.status !== 200 || res.type !== 'basic'){
-            return res;
-        }
-
-        var response = res.clone();
-
-        caches.open(CACHE_VERSION).then(function(cache){
-            cache.put(event.request, response);
-        });
-
-        return res;
+      // Define what we want to cache
+      return cache.addAll([
+        '/',
+        'index.html',
+        'js/app.js',
+        'js/jquery.min.js',
+        'css/style.css',
+        'favicon.ico',
+        'manifest.json',
+        'img/icon-60.png',
+        'img/icon-114.png',
+        'img/icon-152.png'
+      ]);
     })
-}
+  );
+});
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function(keys){
-            return Promise.all(keys.map(function(key, i){
-                if(key !== CACHE_VERSION){
-                    return caches.delete(keys[i]);
-                }
-            }))
-        })
-    )
+// Use ServiceWorker (or not) to fetch data
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+
+    // Look for something in the cache that matches the request
+    caches.match(event.request).then(function(response) {
+
+      // If we find something, return it
+      // Otherwise, use the network instead
+      return response || fetch(event.request);
+    })
+  );
 });
