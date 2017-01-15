efa2eae8a53be47d51398ae68c532f03f3e9d182
Simon Evans <simon.evans@torchbox.com>
2017-01-15 10:02:04 +0000

    Improve note list functionality

--- js/app.js
+++ js/app.js
@@ -3,83 +3,10 @@ if ( 'serviceWorker' in navigator ) {
   navigator.serviceWorker.register( 'sw.js' ).then(function(registration) {
 
     // Registration was successful
-    $( '.console__serviceworker span' ).text( 'ServiceWorker registration successful. Scope: ' + registration.scope )
+    console.log( 'ServiceWorker registration successful. Scope: ' + registration.scope )
   }).catch(function(err) {
 
     // Registration failed with error
-     $( '.console__serviceworker span' ).text( 'ServiceWorker registration failed' + err);
+    console.log( 'ServiceWorker registration failed' + err);
   });
 }
-
-$(document).ready(function () {
-
-    // Simple note list for demo
-    var noteList = function() {
-
-      var $noteList         = $(' .notepad__list' ),
-          $noteListItem     = $( '.notepad__list-item' ),
-          $noteForm         = $( '.notepad__form' ),
-          $noteFormInput    = $( '.notepad__form-input' ),
-          i                 = 0;
-
-      function displayNotes() {
-        for (i = 0; i < localStorage.length; i++) {
-          var noteID = 'task-' + i;
-
-          // Build note list
-          $noteList.append("<li class='notepad__list-item' id='" + noteID + "'>" + localStorage.getItem(noteID) + "</li>");
-        }
-      }
-
-      function storeNote() {
-        if ( $noteFormInput.val() !== '' ) {
-
-            var noteID      = 'task-' + i,
-                task        = $( '#' + noteID ),
-                taskMessage = $noteFormInput.val();
-
-            localStorage.setItem( noteID, taskMessage );
-
-            // Add to note list
-            $noteList.append( "<li class='notepad__list-item' id='" + noteID + "'>" + taskMessage + "</li>" );
-
-            // Reset
-            task.css( 'display', 'none' );
-            task.slideDown();
-            $noteFormInput.val('');
-            i++;
-        }
-      }
-
-      function removeNote( noteItem ) {
-        var noteID    = noteItem.attr( 'id' );
-        
-        localStorage.removeItem( noteID );
-        noteItem.slideUp(300, function () {
-            noteItem.remove();
-        });
-      }
-
-      function bindEvents() {
-
-        // Show any existing notes from localStorage
-        displayNotes();
-
-        // Create new note
-        $noteForm.submit(function() {
-            storeNote();
-            return false;
-        });
-
-        // Remove existing note
-        $noteList.on( 'click', 'li', function () {
-          var item = $(this);
-          removeNote( item );
-        });
-      }
-
-      bindEvents();
-    };
-
-    noteList();
-});
---
+++ js/note-list.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    var noteList = function() {
+
+      var $notepad          = $(' .notepad' ),
+          $noteList         = $(' .notepad__list' ),
+          $noteListItem     = $( '.notepad__list-item' ),
+          $noteForm         = $( '.notepad__form' ),
+          $noteFormInput    = $( '.notepad__form-input' ),
+          $clearList        = $( '.notepad__clear' ),
+          clearListDisplay  = 'notepad__clear--display',
+          i                 = 0,
+          noteCount         = 0;
+
+      function displayNotes() {
+        for (noteCount = 0; noteCount < localStorage.length; noteCount++) {
+          var noteID        = 'task-' + noteCount;
+
+          // Build note list
+          $noteList.append("<li class='notepad__list-item' id='" + noteID + "'>" + localStorage.getItem(noteID) + "</li>");
+
+          // Show reset button
+          $clearList.addClass( clearListDisplay );
+        }
+      }
+
+      function storeNote() {
+        if ( $noteFormInput.val() !== '' ) {
+            var noteID      = 'task-' + noteCount,
+                task        = $( '#' + noteID ),
+                taskMessage = $noteFormInput.val();
+
+            localStorage.setItem( noteID, taskMessage );
+
+            // Add to note list
+            $noteList.append( "<li class='notepad__list-item' id='" + noteID + "'>" + taskMessage + "</li>" );
+
+            // Display reset button
+            if ( !$clearList.hasClass( clearListDisplay ) ) {
+              $clearList.addClass( clearListDisplay );
+            }
+
+            // Reset
+            $noteFormInput.val('');
+            noteCount++;
+        }
+      }
+
+      function clearNotes() {
+
+          // Update DOM
+          $noteList.empty();
+          $clearList.removeClass( clearListDisplay );
+
+          // Clear storage
+          localStorage.clear();
+          noteCount = 0;
+      }
+
+      function bindEvents() {
+
+        // Show any existing notes from localStorage
+        displayNotes();
+
+        // Create new note
+        $noteForm.submit(function() {
+            storeNote();
+            return false;
+        });
+
+        // Reset notes
+        $clearList.on( 'click', function () {
+          clearNotes();
+        });
+      }
+
+      bindEvents();
+    };
+
+    noteList();
+});
